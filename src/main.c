14f29e804f56a424a687ed487d28f6e5740a303b
Shikiyo <shikiyo@archjk>
2022-08-01 11:45:15 -0300

    Running simulation, calculanting time and results

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
 
 #include "types.h"
 #include "args.c"
@@ -36,6 +37,11 @@ ARGS_ArgOption options[] = {
         .type = ARGS_uint,
     },
     [5] = {
+        .small = "",
+        .big = "lcfs",
+        .type = ARGS_flag,
+    },
+    [6] = {
         .small = "v",
         .big = "verbose",
         .type = ARGS_flag,
@@ -54,6 +60,7 @@ typedef struct {
     f64 lambda;
     f64 mu;
     u64 round_count;
+    u8 lcfs;
     u8 verbose;
     u8 ARGS_valid;
 } Options;
@@ -65,6 +72,13 @@ int gen_struct() {
 
 int main(const int argc, const char **argv) {
     Options opts = {
+        .seed = time(NULL),
+        .round_size = 100,
+        .lambda = 0.5,
+        .mu = 1,
+        .round_count = 3200,
+        .lcfs = 0,
+        .verbose = 0,
     };
 
     ARGS_parse(spec, (void*) &opts, sizeof(opts), argc, argv);
@@ -78,9 +92,83 @@ int main(const int argc, const char **argv) {
     printf("    .lambda = %lf\n", opts.lambda);
     printf("    .mu = %lf\n", opts.mu);
     printf("    .round_count = %lu\n", opts.round_count);
+    printf("    .lcfs = %hhu\n", opts.lcfs);
     printf("    .verbose = %hhu\n", opts.verbose);
     printf("    .ARGS_valid = %hhu\n", opts.ARGS_valid);
     printf("}\n");
+
+    // Inicializar sistema
+    RandCtx rand = create_rand_ctx(opts.seed);
+    EventHeap e_heap = init_heap(), *eh = &e_heap;
+    const QueueType qtype = opts.lcfs ? Queue_LCFS : Queue_FCFS;
+    Queue queue = init_queue(qtype), *q = &queue;
+    const Color init_color = 0;
+    System sys = init_system(&rand, opts.lambda, opts.mu,
+            eh, q, init_color),
+           *s = &sys;
+    Stats wt_hat = new_stats(), nq_hat = new_stats();
+    add_first_event(s);
+
+    clock_t clock1 = clock(), clock2 = clock1;
+    // Fase Transiente
+    for ( ; s->wt_stat.n < 1000000; ) {
+        handle_next_event(s);
+    }
+    if ( opts.verbose ) {
+        clock2 = clock() - clock2;
+        printf("transient: %lf seconds (clock)\n",
+                ((f64) clock2) / ((f64) CLOCKS_PER_SEC));
+        clock2 = clock() - clock2;
+    }
+
+    // Rodadas
+    for ( u64 i = 0; i < opts.round_count; i++ ) {
+        next_batch(s);
+        for ( ; s->wt_stat.n < opts.round_size; ) {
+            handle_next_event(s);
+        }
+        const f64 wt_i = discrete_average(s->wt_stat),
+              nq_i = continuous_average(s->nq_stat, s->curr_time);
+        acc_and_update(&wt_hat, wt_i, 1);
+        acc_and_update(&nq_hat, nq_i, 1);
+        if ( opts.verbose ) {
+            clock2 = clock() - clock2;
+            printf("round %4lu: %lf seconds (clock)\n",
+                    i, ((f64) clock2) / ((f64) CLOCKS_PER_SEC));
+            printf("Wait time : %lf\n", wt_i);
+            if ( i > 0 ) {
+                printf("(So far)    average %lf, variance %lf\n",
+                    discrete_average(wt_hat),
+                    discrete_variance(wt_hat));
+            } else {
+                printf("(So far)    average %lf\n",
+                    discrete_average(wt_hat));
+            }
+            printf("Queue size: %lf\n", nq_i);
+            if ( i > 0 ) {
+                printf("(So far)    average %lf, variance %lf\n",
+                    discrete_average(nq_hat),
+                    discrete_variance(nq_hat));
+            } else {
+                printf("(So far)    average %lf\n",
+                    discrete_average(nq_hat));
+            }
+            printf("\n");
+            clock2 = clock() - clock2;
+        }
+    }
+    assert( wt_hat.n == opts.round_count );
+    assert( nq_hat.n == opts.round_count );
+
+    clock1 = clock() - clock1;
+    const char *qtype_name[] = { "FCFS", "LCFS", };
+    printf("Total runtime : %lf seconds (clock)\n",
+            ((f64) clock1) / ((f64) CLOCKS_PER_SEC));
+    printf("Queue type : %s\n", qtype_name[s->queue->type]);
+    printf("Avg Wait time : %lf\n", discrete_average(wt_hat));
+    printf("Var Wait time : %lf\n\n", discrete_variance(wt_hat));
+    printf("Avg Queue size: %lf\n", discrete_average(nq_hat));
+    printf("Var Queue size: %lf\n\n", discrete_variance(nq_hat));
 
     return 0;
 }
